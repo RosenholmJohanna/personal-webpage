9022cc04d06f621b3d671f86f9f47df138d1bec6
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2023-09-21 01:51:32 +0200

    style panel, responsive for mobile and pc

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const burgerIcon = document.getElementById('burger');
+// // const burgerIcon = document.getElementById('burger');
+// const burgerIcon = document.querySelector('.burger');
+
+// const navMenu = document.getElementById('nav');
+
+// burgerIcon.addEventListener('click', () => {
+//   if (navMenu.style.display === 'block') {
+//     navMenu.style.display = 'none';
+//   } else {
+//     navMenu.style.display = 'block';
+//   }
+// });
+
+const burgerIcon = document.querySelector('.burger');
 const navMenu = document.getElementById('nav');
 
 burgerIcon.addEventListener('click', () => {
-  if (navMenu.style.display === 'block') {
+  if (window.getComputedStyle(navMenu).display === 'block') {
     navMenu.style.display = 'none';
   } else {
     navMenu.style.display = 'block';
